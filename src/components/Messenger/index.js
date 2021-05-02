7755b58c1ed388f8f6a0aaba1f5a8aed491b469e
SSaxena2001 <56838945+SSaxena2001@users.noreply.github.com>
2021-05-02 14:06:12 +0530

    "Added sidebar and dialog"

---
+++ src/components/Messenger/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Sidebar from "./SideBar";
+import Dialog from "./Dialog";
+export default function Messenger() {
+  return (
+    <div>
+      <Sidebar />
+      <Dialog />
+    </div>
+  );
+}
